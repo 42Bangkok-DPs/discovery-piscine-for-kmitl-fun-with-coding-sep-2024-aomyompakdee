ae90ef6ccf167b750ac4ee257f9c204e659693b6
aomyompakdee <72595781+aomyompakdee@users.noreply.github.com>
2024-09-12 15:10:33 +0700

    Add files via upload

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,12 @@
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    for i in {1..3}; do
+        if [ $i -le $# ]; then
+            echo "${!i}"
+        else
+            break
+        fi
+    done
+fi
+sh l
